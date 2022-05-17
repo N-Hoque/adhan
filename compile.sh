afc8c3fb597cab27c1d1525f5b64685016d494e5
Naimul Hoque <Naimul_Hoque@outlook.com>
2022-05-17 23:05:27 +0100

    Initial Commit - A culmination of several months worth of work

---
+++ compile.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+
+set -e
+
+# This script is used to compile (and optionally deploy) the Adhan player for a specific ARM architecture
+
+# USAGE:
+
+# ./compile.sh -t 2 -u test -a 192.168.1.96 -d (Build for Raspberry PI 2 and deploy to the PI at IP 192.168.1.96 for test user)
+# ./compile.sh -t 3 -u test -a 192.168.1.96 -d (Same as above but build and deploy for Raspberry PI 3)
+
+while getopts t:u:a:d flag; do
+	case "${flag}" in
+	t) PI_TARGET=${OPTARG} ;;
+	u) USER=${OPTARG} ;;
+	a) IP=${OPTARG} ;;
+	d) DEPLOY="1" ;;
+	*) ;;
+	esac
+done
+
+if [[ $PI_TARGET -gt 2 ]]; then
+	TARGET_IMAGE=adhan-aarch64
+	TARGET_DOCKERFILE=Dockerfile_RPI3
+	TARGET_ARCH=aarch64-unknown-linux-gnu
+elif [[ $PI_TARGET -eq 2 ]]; then
+	TARGET_IMAGE=adhan-armv7
+	TARGET_DOCKERFILE=Dockerfile_RPI2
+	TARGET_ARCH=armv7-unknown-linux-gnueabihf
+else
+	echo "Sorry, this program doesn't currently support the Raspberry PI 0/1"
+	exit 2
+fi
+
+docker build -t ${TARGET_IMAGE} -f ${TARGET_DOCKERFILE} .
+cross build --profile performance --target ${TARGET_ARCH}
+
+if [ -n "$DEPLOY" ]; then
+	if [ -z "$USER" ] || [ -z "$IP" ]; then
+		echo "Must supply USER and IP to deploy"
+		exit 1
+	fi
+	rsync -vihP target/${TARGET_ARCH}/performance/adhan "${USER}@${IP}:adhan_player"
+fi
+
+exit 0
---
+++ src/lib.rs
@@ -0,0 +1,135 @@
+pub mod model;
+
+pub enum AdhanType {
+    Standard,
+    Fajr,
+}
+
+use std::{fs::File, io::BufReader, path::PathBuf};
+
+pub use model::{AdhanCommands, AdhanListSubcommand};
+use model::{AdhanParameters, Method};
+use rodio::{cpal::traits::HostTrait, Decoder, Device, DeviceTrait, OutputStream, Sink};
+use salah::{Coordinates, Local, PrayerSchedule, PrayerTimes};
+
+static SETTINGS_FILE: &str = "settings.yaml";
+static AUDIO_PATH: &str = "audio";
+
+fn get_project_config_directory() -> Option<PathBuf> {
+    directories_next::ProjectDirs::from("", "", "adhan").map(|project_dirs| project_dirs.config_dir().to_path_buf())
+}
+
+#[must_use]
+pub fn read_config() -> AdhanParameters {
+    let Some(config_dir) = get_project_config_directory() else {
+        panic!("AGH")
+    };
+
+    let config_path = config_dir.join(SETTINGS_FILE);
+    let file = File::open(config_path).expect("reading config file");
+
+    serde_yaml::from_reader(file).expect("deserializing config file")
+}
+
+pub fn create_config(method: Method) {
+    let Some(config_dir) = get_project_config_directory() else {
+        panic!("AGH")
+    };
+
+    let config_path = config_dir.join(SETTINGS_FILE);
+    let file = File::create(config_path).expect("opening config file");
+
+    serde_yaml::to_writer(
+        file,
+        &AdhanParameters {
+            coordinates: Coordinates::new(0.0, 0.0),
+            parameters: method.parameters(),
+        },
+    )
+    .expect("serializing config file");
+}
+
+pub fn play_adhan(adhan_type: AdhanType, device: &str) {
+    let Some(config_dir) = get_project_config_directory() else {
+        panic!("AGH")
+    };
+
+    let audio_config_path = config_dir.join(AUDIO_PATH);
+
+    assert!(
+        std::fs::metadata(&audio_config_path).is_ok(),
+        "Audio file for 'Fajr' adhan is not present. Please put one in {}.",
+        audio_config_path.to_string_lossy(),
+    );
+
+    // Get a output stream handle to the default physical sound device
+    let Ok((_stream, stream_handle)) = get_device(device).map_or_else(OutputStream::try_default, |device| {
+        OutputStream::try_from_device(&device)
+    }) else {
+        panic!("finding output device")
+    };
+
+    // Load a sound from a file, using a path relative to Cargo.toml
+    let file = BufReader::new(
+        File::open(if let AdhanType::Standard = adhan_type {
+            audio_config_path.join("normal.mp3")
+        } else {
+            audio_config_path.join("fajr.mp3")
+        })
+        .unwrap(),
+    );
+
+    // Decode that sound file into a source
+    let source = Decoder::new(file).unwrap();
+
+    let sink = Sink::try_new(&stream_handle).unwrap();
+
+    // Add a dummy source of the sake of the example.
+    sink.append(source);
+
+    // The sound plays in a separate thread. This call will block the current thread until the sink
+    // has finished playing all its queued sounds.
+    sink.sleep_until_end();
+}
+
+pub fn list_audio_devices() {
+    let host = rodio::cpal::default_host();
+    if let Ok(devices) = host.output_devices() {
+        for (idx, device) in devices.flat_map(|device| device.name()).enumerate() {
+            println!("{idx}: {device}");
+        }
+    }
+}
+
+pub fn list_audio_hosts() {
+    for (idx, device) in rodio::cpal::available_hosts().iter().enumerate() {
+        println!("{}: {}", idx, device.name());
+    }
+}
+
+#[must_use]
+pub fn new_timetable(parameters: &AdhanParameters) -> PrayerTimes<Local> {
+    PrayerSchedule::new()
+        .with_date(&chrono::Local::now())
+        .with_coordinates(parameters.coordinates())
+        .with_parameters(parameters.parameters())
+        .build()
+        .map_or_else(
+            |err| {
+                eprintln!("Failed to calculate prayer times! - {err}");
+                std::process::exit(1);
+            },
+            |ps| ps,
+        )
+}
+
+fn get_device(device_name: &str) -> Option<Device> {
+    rodio::cpal::default_host()
+        .output_devices()
+        .into_iter()
+        .flatten()
+        .find_map(|dev| {
+            dev.name()
+                .map_or(None, |name| if name == device_name { Some(dev) } else { None })
+        })
+}
